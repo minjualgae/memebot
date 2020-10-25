7601b8206d0c87855e7bd87d16967eaabc808ae4
Jason <30715413+Jason-S-Wu@users.noreply.github.com>
2020-10-25 03:15:33 -0400

    added Programmer Meme Command

--- memebot.py
+++ memebot.py
@@ -74,4 +74,25 @@ async def meme(ctx, *args):
 			pass
 	elif m == ''
 
+@client.command()
+async def pmeme(ctx):
+    redditMemeJSONReseponse = requests.get(
+        url='https://www.reddit.com/r/programmerhumor/new.json', 
+        headers={'user-agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.87 Safari'}
+        )
+
+    memeResponseData = [ ]
+
+    for post in redditMemeJSONReseponse.json()['data']['children']:
+        author = post['data']['author']
+        title = post['data']['title']
+        imageurl = post['data']['url']
+        if '.png' in imageurl or '.gif' in imageurl or '.jpg' in imageurl or '.jpeg' in imageurl:
+            memeResponseData.append([author, title, imageurl])
+
+    randomMeme = random.choice(memeResponseData)
+
+    embed=discord.Embed(title=randomMeme[1], description=f'u/{randomMeme[0]}', color=0x0C0CCC).set_image(url=randomMeme[2])
+    await ctx.channel.send(embed=embed)
+
 client.run(token)
