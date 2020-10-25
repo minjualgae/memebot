c1349a9facff9ab554864edc5a081384fd11d0a5
jakeryan726 <73397921+jakeryan726@users.noreply.github.com>
2020-10-25 01:38:19 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ from discord.ext import commands
 client = commands.Bot(command_prefix = "~")
 
 @client.command()
-async def meme(ctx, img, name):
+async def meme(ctx, img, *, name):
     image = Image.open(img)
     cv2.putText(image, name,(10,500 offset), font, 1,(0,0,0),2)
     await channel.send(file=discord.File(image))
