b9a8e5b9f1e239dac79ba5ffa5a7eba033d9f46a
Jason-S-Wu <30715413+Jason-S-Wu@users.noreply.github.com>
2020-10-25 03:19:57 -0400

    Add files via upload

---
+++ redditmemes.py
@@ -0,0 +1,36 @@
+import requests
+import random
+import discord
+from discord.ext import commands
+
+client = commands.Bot(command_prefix = '$', activity = discord.Game("Starting Bot"), status='online')
+client.remove_command('help')
+
+@client.event
+async def on_ready():
+    print('Bot Ready')
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.playing, name="Memes"))
+
+@client.command()
+async def pmeme(ctx):
+    redditMemeJSONReseponse = requests.get(
+        url='https://www.reddit.com/r/programmerhumor/new.json', 
+        headers={'user-agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.87 Safari'}
+        )
+
+    memeResponseData = [ ]
+
+    for post in redditMemeJSONReseponse.json()['data']['children']:
+        author = post['data']['author']
+        title = post['data']['title']
+        imageurl = post['data']['url']
+        if '.png' in imageurl or '.gif' in imageurl or '.jpg' in imageurl or '.jpeg' in imageurl:
+            memeResponseData.append([author, title, imageurl])
+
+    randomMeme = random.choice(memeResponseData)
+
+    embed=discord.Embed(title=randomMeme[1], description=f'u/{randomMeme[0]}', color=0x0C0CCC).set_image(url=randomMeme[2])
+    await ctx.channel.send(embed=embed)
+    
+
+client.run(token)
